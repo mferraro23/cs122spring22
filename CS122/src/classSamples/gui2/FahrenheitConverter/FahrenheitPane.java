0730f3979173f522bc27e7f6bb86b6fcf02b10df
Michael Ferraro <mf69386p@pace.edu>
2022-04-05 12:19:24 -0400

    updates

--- CS122/src/classSamples/gui2/FahrenheitConverter/FahrenheitPane.java
+++ CS122/src/classSamples/gui2/FahrenheitConverter/FahrenheitPane.java
@@ -9,8 +9,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
-import java.io.IOException;
-
 //************************************************************************
 //  FahrenheitPane.java       Author: Lewis/Loftus
 //

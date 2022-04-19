5cbb136b23f0c952e462fe5a83c32a379b0563db
Michael Ferraro <mf69386p@pace.edu>
2022-04-19 15:53:58 -0400

    updates

--- CS122/src/midterm2Review/Recursion.java
+++ CS122/src/midterm2Review/Recursion.java
@@ -2,27 +2,27 @@ package midterm2Review;
 
 public class Recursion {
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	public static int factorial(int num) {
 		//base case
-		if(num==1)
+		if (num == 1)
 			return 1;
-		// recursive case
+			// recursive case
 		else
-			return num * factorial(num-1);
+			return num * factorial(num - 1);
 	}
-	
+
 	public static int indirectFactorial(int num) {
 		//base case
-		if(num==1)
+		if (num == 1)
 			return 1;
-		//indirect recursive case
+			//indirect recursive case
 		else
 			return num * decrementAndFactorial(num);
 	}
-	
+
 	public static int decrementAndFactorial(int num) {
 		num = num - 1;
 		return indirectFactorial(num);
@@ -35,14 +35,5 @@ public class Recursion {
 	recursive case
 	base case
  */
-=======
-
-	in recursion
-	recursive case
-	base case
-
-	 */
-
 }
->>>>>>> Stashed changes
 

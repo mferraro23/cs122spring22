35548c0be5b89b6141d66cd1be24f35828885fe5
Michael Ferraro <mf69386p@pace.edu>
2022-04-28 21:50:07 -0400

    Created Files

---
+++ CS122/src/assignments/assignment6/LinkedStack/LinkedStack.java
@@ -0,0 +1,42 @@
+package assignments.assignment6.LinkedStack;
+
+import java.util.Stack;
+
+public class LinkedStack implements StackADT {
+    // ---------------------------------------------------
+    // Constructor -- initializes top
+    // ---------------------------------------------------
+    public LinkedStack(){
+
+    }
+    // ---------------------------------------------------
+    // Adds element to top of stack if it's not full, else
+    // does nothing.
+    // ---------------------------------------------------
+    @Override
+    public void push(Object val) {
+
+    }
+    // ---------------------------------------------------
+    // Removes and returns value at top of stack. If stack
+    // is empty returns null.
+    // ---------------------------------------------------
+    @Override
+    public Object pop() {
+        return null;
+    }
+    // ---------------------------------------------------
+    // Returns true if stack is empty, false otherwise.
+    // ---------------------------------------------------
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+    // ---------------------------------------------------
+    // Returns true if stack is full, false otherwise.
+    // ---------------------------------------------------
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+}
---
+++ CS122/src/assignments/assignment6/LinkedStack/StackTest.java
@@ -0,0 +1,31 @@
+package assignments.assignment6.LinkedStack;
+
+public class StackTest {
+    public static void main(String[] args)
+    {
+        StackADT stack = new LinkedStack();
+
+        //push some stuff on the stack
+        for (int i=0; i<10; i++) {
+            stack.push(i * 2);
+        }
+
+        //pop and print
+        //should print 18 16 14 12 10 8 6 4 2 0
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+            System.out.println();
+        }
+
+        //push a few more things
+        for (int i=1; i<=6; i++) {
+            stack.push(i);
+        }
+        
+        //should print 6 5 4 3 2 1
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+            System.out.println();
+        }
+    }
+}

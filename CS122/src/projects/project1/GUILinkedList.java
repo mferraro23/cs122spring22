a87b3fc04f8f5388af7484eacff3e287d4c4213c
Michael Ferraro <mf69386p@pace.edu>
2022-05-06 14:53:16 -0400

    bug fixes

--- CS122/src/projects/project1/GUILinkedList.java
+++ CS122/src/projects/project1/GUILinkedList.java
@@ -1,25 +1,18 @@
 package projects.project1;
 
 import javafx.application.Application;
-
 import javafx.scene.Scene;
-
 import javafx.scene.control.Button;
-
 import javafx.scene.control.TextField;
-
 import javafx.scene.layout.HBox;
-
 import javafx.scene.layout.VBox;
-
 import javafx.stage.Stage;
 
 public class GUILinkedList extends Application {
 
-    private LinkedList<String> list = new LinkedList<>();
+    private final LinkedList<String> list = new LinkedList<>();
 
     @Override
-
     public void start(Stage primaryStage) {
 
         TextField textField = new TextField();
@@ -31,71 +24,41 @@ public class GUILinkedList extends Application {
         Button clearButton = new Button("Clear");
 
         addButton.setOnAction(e -> {
-
             String value = textField.getText();
-
             list.add(value);
-
             textField.clear();
-
         });
 
         removeButton.setOnAction(e -> {
-
             list.remove();
-
         });
 
         clearButton.setOnAction(e -> {
-
             list.clear();
-
         });
 
         HBox hBox = new HBox(10);
-
         hBox.getChildren().addAll(textField, addButton, removeButton, clearButton);
-
         VBox vBox = new VBox(10);
-
         vBox.getChildren().addAll(list, hBox);
-
         Scene scene = new Scene(vBox, 400, 400);
-
         primaryStage.setScene(scene);
-
         primaryStage.show();
-
     }
-
-    public static void main(String[] args) {
-
-        launch(args);
-
-    }
-
+    public static void main(String[] args) {launch(args);}
 }
 
 class LinkedList<T> extends VBox {
-
     private Node<T> head;
-
     private Node<T> tail;
-
     public LinkedList() {
-
         super(10);
-
         this.head = null;
-
         this.tail = null;
-
     }
 
     public void add(T value) {
-
         Node<T> node = new Node<>(value);
-
         if (head == null) {
             head = node;
         } else {
@@ -104,9 +67,7 @@ class LinkedList<T> extends VBox {
         tail = node;
         this.getChildren().add(node);
     }
-
     public void remove() {
-
         if (head == null) {
             throw new RuntimeException("List is empty");
         }
@@ -117,78 +78,45 @@ class LinkedList<T> extends VBox {
             tail = null;
         }
     }
-
     public void clear() {
         this.getChildren().clear();
         head = null;
         tail = null;
     }
     @Override
-
     public String toString() {
-
         if (head == null) {
-
             return "null";
-
         }
         return head.toString();
     }
 }
-
 class Node<T> extends HBox {
-
     private T value;
     private Node<T> next;
-
     public Node(T value) {
-
         super(10);
-
         this.value = value;
-
         this.next = null;
-
         this.getChildren().add(new Button(value.toString()));
-
     }
-
     public T getValue() {
-
         return value;
-
     }
-
     public void setValue(T value) {
-
         this.value = value;
-
     }
-
     public Node<T> getNext() {
-
         return next;
-
     }
-
     public void setNext(Node<T> next) {
-
         this.next = next;
-
     }
-
     @Override
-
     public String toString() {
-
         if (next == null) {
-
             return value + " -> null";
-
         }
-
         return value + " -> " + next.toString();
-
     }
-
 }
